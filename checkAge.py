e789a21d31a11c54729cc2061acd43c587b95f0a
John jahbless <Johnjahbless@gmail.com>
2018-06-05 17:46:50 +0100

    task completed

---
+++ checkAge.py
@@ -0,0 +1,6 @@
+name = input("Enter your name")
+age = int (input ("Enter your age"))
+ThisYear = 2018
+BirthDay = ThisYear - age
+Age50 = BirthDay + 50
+print ("Hello %s you will be 50years old by %d " %( name, Age50))
